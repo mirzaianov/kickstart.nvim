630a588418f4102c9206db5756693775a0354b59
Riaz Mirzaianov <87698216+mirzaianov@users.noreply.github.com>
2026-01-21 19:25:19 +0300

    fix: Disable line under cursor

--- init.lua
+++ init.lua
@@ -157,6 +157,7 @@ vim.o.inccommand = 'split'
 
 -- Show which line your cursor is on
 vim.o.cursorline = true
+vim.o.cursorlineopt = 'number' -- Current line background is NOT highlighted
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.o.scrolloff = 10
